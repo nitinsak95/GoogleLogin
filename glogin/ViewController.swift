ddf7669779cecb90b3cd71d441d43e7f812c24c9
affixusimac1 <viveking@outlook.com>
2018-04-13 14:49:56 +0530

    google login

--- glogin/ViewController.swift
+++ glogin/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import GoogleSignIn
+import Google
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
 
+    @IBOutlet weak var labelUserEmail: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var error: NSError?
+        
+         GGLContext.sharedInstance().configureWithError(&error)
+        
+        if error != nil{
+            print(error ?? "google error")
+            return
+        } 
+        
+        
+        //adding the delegates
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        let googleSignInButton = GIDSignInButton()
+        googleSignInButton.center = view.center
+        view.addSubview(googleSignInButton)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        //if any error stop and print the error
+        if error != nil{
+            print(error ?? "google error")
+            return
+        }
+        
+        //if success display the email on label
+        labelUserEmail.text = user.profile.name
+        
     }
-
-
+    
 }
 
